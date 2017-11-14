51c96fe4ca3b4b2e104102fcd32c6dc455214ade
VifillS <vis32@hi.is>
2017-11-14 01:16:42 +0000

    Toolbar ordinn functional

--- player.js
+++ player.js
@@ -57,6 +57,7 @@ const videos = `{
   ]
 }`;
 
+
 class Player {
   /**
    * Finnur container fyrir myndbönd og form.
@@ -65,8 +66,8 @@ class Player {
   constructor() {
     this.id = this.getQueryVariable('id');
     this.container = document.querySelector('.player');
-    this.toolbar = document.querySelector('.tools');
     this.videos = JSON.parse(videos).videos;
+    this.video;
 
     // gamala
     //    this.keyName = 'countdown';
@@ -95,216 +96,74 @@ class Player {
    * og thumbnails
    */
   load() {
-    const video = this.videos[this.id];
+    const video = this.videos[this.id - 1];
+
+    const headEl = document.createElement('h1');
+    headEl.appendChild(document.createTextNode(video.title));
+    headEl.setAttribute('class','title');
+    document.querySelector('main').prepend(headEl);
 
     const vidEl = document.createElement('video');
     vidEl.setAttribute('src', video.video);
-    // vidEl.setAttribute('autoplay','True');
+    vidEl.setAttribute('poster', video.poster);
     vidEl.setAttribute('controls', 'True');
+    //this.video = vidEl;
     this.container.prepend(vidEl);
 
-    // const savedData = window.localStorage.getItem(this.keyName);
-
-    //    if (savedData) {
-    //      const parsed = JSON.parse(savedData);
-    //      const date = new Date(parsed.date);
-    //
-    //      this.create(parsed.title, date);
-    //    }
+    // setja eventlistenera
+    document.getElementById('playPause').addEventListener("click", this.playPause);
+    document.getElementById('muteUnmute').addEventListener("click", this.muteUnmute);
+    document.getElementById('fullscreen').addEventListener("click", this.fullscreen);
+    document.getElementById('forward').addEventListener("click", this.forward);
+    document.getElementById('back').addEventListener("click", this.back);
   }
 
-  /**
-   * Tekur við title sem streng og date sem Date hlut
-   * Vistar sem json gögn í localStorage undir this.keyName
-   */
-  //  save(title, date) {
-  //    const data = { title, date };
-  //    const json = JSON.stringify(data);
-  //
-  //    window.localStorage.setItem(this.keyName, json);
-  //  }
-  //
-  //  /**
-  //   * Handler fyrir submit á formi.
-  //   * Sækir gögn úr formi og kallar í this.parseDate()
-  //   * Vistar gögn með this.save() og sýnir niðurteljara með this.create()
-  //   */
-  //  submit(e) {
-  //    e.preventDefault();
-  //
-  //    const title = this.form.querySelector('input[type=text]');
-  //    const dags = this.form.querySelector('input[type=date]');
-  //    const time = this.form.querySelector('input[type=time]');
-  //    const date = this.parseDate(dags.value, time.value);
-  //
-  //    console.log('titill er', title.value);
-  //    console.log('date er', date.toString());
-  //
-  //    this.save(title.value, date);
-  //
-  //    this.load();
-  //  }
-  //
-  //  /**
-  //   * Tekur við:
-  //   *  - date sem streng á forminu "yyyy-mm-dd", t.d. "2017-11-06"
-  //   *  - time sem streng á forminu "hh:mm", t.d. "09:00"
-  //   * Skilar date hlut með gögnum úr date og time
-  //   */
-  //  parseDate(date, time) {
-  //    return new Date(`${date}T${time}:00`);
-  //  }
-  //
-  //  /**
-  //   * Býr til element fyrir niðurteljara og bætir við this.container
-  //   * Setur this.date sem dagsetningu sem talið er niður að
-  //   * Setur this.element sem element sem geymir niðurteljara
-  //   * Bætir við "eyða" takka sem sér um að eyða niðurteljara með this.delete
-  //   * Byrjar niðurteljara með this.startCounter() og
-  //   * felur form með this.hideForm()
-  //   */
-  //  create(title, date) {
-  //    this.date = date;
-  //
-  //    const h2 = document.createElement('h2');
-  //    h2.appendChild(document.createTextNode(title));
-  //    h2.classList.add('countdown__heading');
-  //    this.container.appendChild(h2);
-  //
-  //    const countdown = document.createElement('div');
-  //    this.container.appendChild(countdown);
-  //    this.element = countdown;
-  //
-  //    const deleteButton = document.createElement('button');
-  //    deleteButton.appendChild(document.createTextNode('Eyda'));
-  //    deleteButton.classList.add('button');
-  //    deleteButton.addEventListener('click', this.delete.bind(this));
-  //    this.container.appendChild(deleteButton);
-  //
-  //    this.startCounter();
-  //    this.hideForm();
-  //  }
-  //
-  //  /**
-  //   * Felur form með CSS
-  //   */
-  //  hideForm() {
-  //    this.form.classList.add('form__hidden');
-  //  }
-  //
-  //  /**
-  //   * Sýnir form með CSS
-  //   */
-  //  showForm() {
-  //    this.form.classList.remove('form__hidden');
-  //  }
-  //
-  //  /**
-  //   * Byrjar niðurteljara með this.count() og keyrir á 1000ms fresti
-  //   * með window.setInterval og setur id á teljara í this.interval
-  //   */
-  //  startCounter() {
-  //    this.count();
-  //    this.interval = window.setInterval(this.count.bind(this), 1000);
-  //  }
-  //
-  //  /**
-  //   * Stöðvar teljara með window.clearInterval á this.interval
-  //   */
-  //  stopCounter() {
-  //    window.clearInterval(this.interval);
-  //  }
-  //
-  //  /**
-  //   * Býr til element sem heldur utan um teljara, á forminu:
-  //   * <div class="countdown__box">
-  //   *   <span class="countdown__num">num</span>
-  //   *   <span class="countdown__title">title</span>
-  //   * </div>
-  //   * og skilar element
-  //   */
-  //  createElement(num, title) {
-  //    const el = document.createElement('div');
-  //    el.classList.add('countdown__box');
-  //
-  //    const numEl = document.createElement('span');
-  //    numEl.classList.add('countdown__num');
-  //    numEl.appendChild(document.createTextNode(num));
-  //    el.appendChild(numEl);
-  //
-  //    const titleEl = document.createElement('span');
-  //    titleEl.classList.add('countdown__title');
-  //    titleEl.appendChild(document.createTextNode(title));
-  //    el.appendChild(titleEl);
-  //
-  //    return el;
-  //  }
-  //
-  //  /**
-  //   * Eyðir niðurteljara með því að fjarlægja úr localStorage og
-  //   * fjarlægja allt úr this.container.
-  //   * Kallar líka í this.stopCounter() og this.showForm()
-  //   */
-  //  delete() {
-  //    window.localStorage.removeItem(this.keyName);
-  //
-  //    this.stopCounter();
-  //    this.showForm();
-  //
-  //    while (this.container.firstChild) {
-  //      this.container.removeChild(this.container.firstChild);
-  //    }
-  //  }
-  //
-  //  /**
-  //   * Tekur við remaining sem eru millisekúndur í dagsetningu sem talið er
-  //   * niður í.
-  //   * Útbýr og skilar element sem inniheldur element fyrir daga, klukkustundir,
-  //   * mínútur og sekúndur þar til remaining gerist. Hver „partur“ er búinn til
-  //   * með kalli í this.createElement
-  //   */
-  //  countdown(remaining) {
-  //    const totalSecs = remaining / 1000;
-  //
-  //    const days = Math.floor(totalSecs / (60 * 60 * 24));
-  //    const hours = Math.floor(totalSecs / (60 * 60)) % 24;
-  //    const mins = Math.floor((totalSecs / 60) % 60);
-  //    const secs = Math.floor(totalSecs % 60);
-  //
-  //    const container = document.createElement('div');
-  //    container.classList.add('countdown__container');
-  //
-  //    container.appendChild(this.createElement(days, 'Dagar'));
-  //    container.appendChild(this.createElement(hours, 'Klst'));
-  //    container.appendChild(this.createElement(mins, 'Min'));
-  //    container.appendChild(this.createElement(secs, 'Sek'));
-  //
-  //    return container;
-  //  }
-  //
-  //  /**
-  //   * Telur niður.
-  //   * Fjarlægir allt úr this.element (ef eitthvað er þar) og athugar síðan hvort
-  //   * this.date (dagsetning sem talið er niður að) sé liðin og ef svo er birtir
-  //   * textann "Komið!" og stoppa teljara með this.stopCounter()
-  //   * Ef ekki liðið uppfærir teljara með því að bæta element úr this.countdown()
-  //   * við this.element
-  //   */
-  //  count() {
-  //    if (this.element.firstChild) {
-  //      this.element.removeChild(this.element.firstChild);
-  //    }
-  //
-  //    const diff = this.date - new Date();
-  //
-  //    if (diff <= 0) {
-  //      this.element.appendChild(document.createTextNode('Komid!'));
-  //      this.stopCounter();
-  //    } else {
-  //      const countdown = this.countdown(diff);
-  //      this.element.appendChild(countdown);
-  //    }
-  //  }
+  // TODO setja video sem class attibute
+
+  playPause() {
+    const video = document.querySelector('video');
+    if (video.paused) {
+      video.play();
+      document.getElementById('playPause').setAttribute('src','./img/pause.svg');
+    } else {
+      video.pause();
+      document.getElementById('playPause').setAttribute('src','./img/play.svg');
+    }
+  }
+
+  muteUnmute() {
+    const video = document.querySelector('video');
+    if (video.muted) {
+      video.muted = false;
+      document.getElementById('muteUnmute').setAttribute('src','./img/mute.svg');
+    } else {
+      video.muted = true;
+      document.getElementById('muteUnmute').setAttribute('src','./img/unmute.svg');
+    }
+  }
+
+  // tekid af netinu - endurskoda en samt ekki
+  fullscreen() {
+    const video = document.querySelector('video');
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) {
+      video.mozRequestFullScreen(); // Firefox
+    } else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen(); // Chrome and Safari
+    }
+  }
+
+  forward() {
+    const video = document.querySelector('video');
+    video.currentTime += 15;
+  }
+
+  back() {
+    const video = document.querySelector('video');
+    video.currentTime -= 15;
+  }
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
